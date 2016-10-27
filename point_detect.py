1fdb8ad07f35435eb1fe993639d8fcb82ca1ec53
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2016-10-27 12:30:43 +0300

    one-to-one matching of images complete

--- point_detect.py
+++ point_detect.py
@@ -5,15 +5,18 @@
 
 import numpy as np
 import cv2
+import os
 #from matplotlib import pyplot as plt
 
 
 def run():
-	img1 = cv2.imread('field_photos/DJI_0415.jpg',0)
-	img2 = cv2.imread('field_photos/DJI_0416.jpg',0)
+	my_path = 'field_photos/'
+	photos = [ f for f in os.listdir(my_path) if f[-3:]=='JPG' or f[-3:]=='jpg' ]
+	print photos
 
-	return get_matched_points(img1,img2)
-
+	[ get_matched_points(my_path+photos[i],my_path+photos[i+1]) for i in range(len(photos)-1) ]
+	
+	
 
 
 def pyrDown(img,N):
@@ -22,7 +25,10 @@ def pyrDown(img,N):
 
 
 
-def get_matched_points(img1,img2):
+def get_matched_points(photo1,photo2):
+	img1 = cv2.imread(photo1,0)
+	img2 = cv2.imread(photo2,0)
+
 	img1 = pyrDown(img1,3)
 	img2 = pyrDown(img2,3)
 
@@ -41,7 +47,10 @@ def get_matched_points(img1,img2):
 
 	# Sort them in the order of their distance.
 	matches = sorted(matches, key = lambda x:x.distance)
-
+	print "\n", photo1,photo2
+	print [ m.distance for m in matches]
+	# imgIdx', 'queryIdx'
+	"""
 	# Draw first 10 matches.
 	img3 = img1.copy()
 	img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10], img3,flags=2)
@@ -49,6 +58,7 @@ def get_matched_points(img1,img2):
 	cv2.imshow('matches',img3)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
+	"""
 
 
 if __name__ == "__main__":
